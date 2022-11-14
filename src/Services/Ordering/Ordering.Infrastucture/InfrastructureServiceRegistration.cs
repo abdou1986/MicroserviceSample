d3823881fb6ec823c0b891428d52225251373ead
Abdelkarim <User@DESKTOP-8C0P2JM>
2022-11-14 15:43:13 +0300

    Add Ordering Application with CQRS and Clean Architecture

---
+++ src/Services/Ordering/Ordering.Infrastucture/InfrastructureServiceRegistration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Models;
+using Ordering.Infrastucture.Mail;
+using Ordering.Infrastucture.Persistence;
+using Ordering.Infrastucture.Repositories;
+
+
+namespace Ordering.Infrastucture
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<OrderContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("OrderingConnectionString")));
+
+            //Using typeof it is for MediatR
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
+
+            //here we use AddTransient because we need a one instance when we lanch the application
+            services.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastucture/Persistence/OrderContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Common;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastucture.Persistence
+{
+    public class OrderContext : DbContext
+    {
+        //1- Add DbContextOptions<ClassContext> as injection inside the ctor
+        //2- should add inheritence from base
+
+        public OrderContext(DbContextOptions<OrderContext> options ) : base( options )
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+
+            foreach (var entry in ChangeTracker.Entries<EntityBase>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        entry.Entity.CreatedBy = "swn";
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        entry.Entity.LastModifiedBy = "swn";
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+    }
+}
